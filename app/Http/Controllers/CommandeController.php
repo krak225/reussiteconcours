e3f2dbd984e6b27acdf558e73952b5433e471e34
Armand Kouassi <krak225@gmail.com>
2022-11-29 16:43:38 +0000

    up

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -128,7 +128,7 @@ class CommandeController extends Controller
     public function commandes()
     {
 		
-		$commandes = Commande::where(['user_id'=>Auth::user()->id])->get();
+		$commandes = Commande::where(['user_id'=>Auth::user()->id])->get()->sortBy('commande_id');
 		
 		return view('shop.commandes', ['commandes'=>$commandes]);
 		
